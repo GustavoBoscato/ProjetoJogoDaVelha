a6bec5f1980afe5a916f398d08015b271f01f74e
Gustavo Boscato <curiosoxcdf@gmail.com>
2025-09-19 15:14:26 -0300

    visual basico

--- JogoDaVelha/App.js
+++ JogoDaVelha/App.js
@@ -3,18 +3,45 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.tudo}>
+      <View style={styles.cabecalho}>
+        <Text>Jogo da Velha</Text>
+        <Text>Vez do Jogador:</Text>
+      </View>
+      <View style={styles.celula}>
+
+      </View>
+      <View style={styles.rodape}>
+        <Text>Histórico de jogadas:</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  tudo: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#d8d4d4ff',
+    
+  },
+
+  cabecalho: {
+    flex: 0.2,
+    justifyContent: 'center',
+    alignItems:'center',
+    backgroundColor: "#4e4d4dff"
+  },
+  celula: {
+    flex: 0.6,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: "#c0c0c0ff"
+  },
+  rodape: {
+    flex: 0.2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: "#4e4d4dff"
+    
   },
 });
---
+++ VitoriaGustavo/Cabecalho.js
@@ -0,0 +1,7 @@
+import { View } from "react-native-web"
+
+const Cabecalho = () => {
+    return (
+        <View></View>
+    )
+}
